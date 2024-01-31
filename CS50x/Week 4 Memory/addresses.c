ebef3eefc7ecc8ff06c7893d6be65adde2f110e2
Thiri <109120938+ThiriYaminHsu@users.noreply.github.com>
2024-01-31 10:48:41 +0630

    Update addresses.c

--- CS50x/Week 4 Memory/addresses.c
+++ CS50x/Week 4 Memory/addresses.c
@@ -1,3 +1,4 @@
+'''c
 #include <stdio.h>
 
 // This program demonstrates the use of pointers in C.
@@ -11,3 +12,7 @@ int main(void)
     // Print the value stored at the address pointed to by 'p'.
     printf("%i\n", *p);
 }
+
+a
+
+'''c
