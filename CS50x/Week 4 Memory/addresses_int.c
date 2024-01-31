1c14f65833c9709defddd6c743e7c7a431f8a760
Thiri <109120938+ThiriYaminHsu@users.noreply.github.com>
2024-01-31 11:20:32 +0630

    Update addresses_int.c

--- CS50x/Week 4 Memory/addresses_int.c
+++ CS50x/Week 4 Memory/addresses_int.c
@@ -3,11 +3,14 @@
 // This program demonstrates the use of pointers in C.
 int main(void)
 {
+    // We start by declaring an integer variable 'n' and assigning it the value 50.
     int n = 50;
   
-    // Declare a pointer 'p' and assign the address of 'n' to it.
+    // Now, let's introduce a pointer called 'p'. A pointer is a variable that stores the memory address of another variable.
+    // Here, we're declaring a pointer 'p' and assigning it the address of our integer variable 'n' using the '&' (address-of) operator.
     int *p = &n;
 
-    // Print the value stored at the address pointed to by 'p'.
+    // We use the pointer 'p' to access the value stored at the memory address it points to.
+    // This is done with the '*' (dereference) operator. So, when we print '*p', we're printing the value stored at the address pointed to by 'p'.
     printf("%i\n", *p);
 }
